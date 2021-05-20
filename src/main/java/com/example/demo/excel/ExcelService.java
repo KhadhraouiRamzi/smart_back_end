f7265d4b01d82c87f4172c700704858f76813279
majdiCh016 <majdi.chouat@esprit.tn>
2021-05-20 09:36:39 +0100

    commit of upload file orange

--- src/main/java/com/example/demo/excel/ExcelService.java
+++ src/main/java/com/example/demo/excel/ExcelService.java
@@ -10,7 +10,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -51,6 +54,22 @@ public class ExcelService {
 
             List<details> tutorials = new ArrayList<>();
 
+            /*-------parsing of date from ---------*/
+            String fdate1=(sheet.getRow(2).getCell(0)).getStringCellValue();
+            String  str1 = fdate1.replaceAll("([^/0-9])", "");
+            System.out.println(str1);
+            Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(str1);
+            java.sql.Date d1=  new java.sql.Date(date1.getTime());
+            System.out.println(d1);
+
+            /*-------parsing of date to ---------*/
+            String tdate2=(sheet.getRow(3).getCell(0)).getStringCellValue();
+            String  str2 = tdate2.replaceAll("([^/0-9])", "");
+            System.out.println(str2);
+            Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(str2);
+            java.sql.Date d2=  new java.sql.Date(date2.getTime());
+            System.out.println(date2);
+
             int rowNumber = 0;
             while (rows.hasNext()) {
                 Row currentRow = rows.next();
@@ -70,6 +89,9 @@ public class ExcelService {
                 int cellIdx = 0;
                 while (cellsInRow.hasNext()) {
                     Cell currentCell = cellsInRow.next();
+
+                    details.setDate1(d1);
+                    details.setDate2(d2);
 
                     switch (cellIdx) {
                         case 0:
@@ -103,7 +125,7 @@ public class ExcelService {
 
             return tutorials;
 
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             throw new RuntimeException("fail to parse Excel file: " + e.getMessage());
         }
     }
